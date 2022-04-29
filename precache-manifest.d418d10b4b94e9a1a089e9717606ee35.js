6b439d8693cf8732c2669ce517638f8993a909c6
KristersgCode <kristersgutmanis@gmail.com>
2022-04-29 08:00:43 +0300

    Updates

---
+++ precache-manifest.d418d10b4b94e9a1a089e9717606ee35.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0457884c33c3a6596849d835b1b130",
+    "url": "/MathWebsite/index.html"
+  },
+  {
+    "revision": "ced12b24f1047459cb5b",
+    "url": "/MathWebsite/static/css/main.705d6f82.chunk.css"
+  },
+  {
+    "revision": "1264e65067b2f786c53a",
+    "url": "/MathWebsite/static/js/2.56e07b40.chunk.js"
+  },
+  {
+    "revision": "b2871250ed127b512992",
+    "url": "/MathWebsite/static/js/3.6f35523b.chunk.js"
+  },
+  {
+    "revision": "ced12b24f1047459cb5b",
+    "url": "/MathWebsite/static/js/main.ce474d2d.chunk.js"
+  },
+  {
+    "revision": "fb997f4581c4f2cada12",
+    "url": "/MathWebsite/static/js/runtime~main.5b89a431.js"
+  },
+  {
+    "revision": "d5d99716739ed00a508500d30eb5a666",
+    "url": "/MathWebsite/static/media/svg-1.1.d5d99716.svg"
+  },
+  {
+    "revision": "61e17969d70c5cad1d5e87b6fc4bda0a",
+    "url": "/MathWebsite/static/media/svg-1.61e17969.svg"
+  },
+  {
+    "revision": "0404e535e3516e404cde634f4a12773c",
+    "url": "/MathWebsite/static/media/svg-3.0404e535.svg"
+  },
+  {
+    "revision": "eccde3e7dd0b63da6ab630331b42781c",
+    "url": "/MathWebsite/static/media/svg-4.eccde3e7.svg"
+  },
+  {
+    "revision": "5ef91800c46677c53ff62acf175a93b3",
+    "url": "/MathWebsite/static/media/video2.5ef91800.mp4"
+  }
+]);
